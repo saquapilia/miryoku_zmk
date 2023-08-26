d1475f1b3af88e0bc4284a7aac10ef2c744d25f5
Christina Palm <christina.palm@kitchenstories.de>
2023-08-26 15:15:58 +0200

    Update glove80.h to add custom spotlight key

--- miryoku/mapping/80/glove80.h
+++ miryoku/mapping/80/glove80.h
@@ -5,6 +5,8 @@
 
 #define XXX &none
 #define C00 &kp LS(LG(NUMBER_5))
+#define C01 &kp LG(SPACE)
+
 
 #define MIRYOKU_LAYOUTMAPPING_GLOVE80( \
      K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
@@ -16,7 +18,7 @@ XXX  XXX  XXX  XXX  XXX                                               XXX  XXX  
 XXX  XXX  XXX  XXX  XXX  XXX                                     XXX  XXX  XXX  XXX  XXX  XXX \
 XXX  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  C00  XXX  XXX       XXX  XXX  XXX  K25  K26  K27  K28  K29  XXX \
+XXX  K20  K21  K22  K23  K24  C00  XXX  XXX       XXX  XXX  C02  K25  K26  K27  K28  K29  XXX \
 XXX  XXX  XXX  XXX  XXX       K32  K33  K34       K35  K36  K37       XXX  XXX  XXX  XXX  XXX
 
 #endif
